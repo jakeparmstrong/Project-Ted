13e0f8f79e6f70974a7c019faeca93142d2bec6d
hake <jake.p.armstrong@gmail.com>
2020-04-28 15:13:40 -0400

    fixed jump; changed tileset

--- Ted/Ted.gd
+++ Ted/Ted.gd
@@ -1,3 +1,4 @@
+
 extends KinematicBody2D
 
 var moving_right
@@ -6,10 +7,10 @@ var walk_speed = 350
 var walk_accel = 1000
 var gravity_scale = 800
 var jump_velocity = 100
-var jump_impulse = 3000
 var jump_multiplier = 3
 var jump_button_timer = 0
 var allow_jump = true
+var max_jump_length_s = 1.5
 
 
 func _ready() -> void:
@@ -34,15 +35,15 @@ func get_input(delta):
 		$AnimatedSprite.set_flip_h(false)
 	if Input.is_action_pressed("player_jump") and jump_button_timer < 0.2 and allow_jump:
 		jump_button_timer += delta
-		velocity.y -= gravity_scale * (jump_multiplier - 1) #add impulse thing?
-		print(velocity)
-	if jump_button_timer > 0.2:
+		velocity.y -= gravity_scale * (jump_multiplier - 1)
+	if jump_button_timer > max_jump_length_s:
 		allow_jump = false
+		print("deltahi")
 
 func _physics_process(delta: float) -> void:
 	velocity.y = gravity_scale
 	get_input(delta)
 	move_and_slide(velocity, Vector2(0, -1))
-	if is_on_floor() and Input.is_action_just_released("player_jump"): #doesn't work -- check if ted is on floor?
+	if is_on_floor() and !Input.is_action_pressed("player_jump"): 
 		allow_jump = true
 		jump_button_timer = 0
