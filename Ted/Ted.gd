9230ecbce203d88b1fe25c5c01dd7eb8cb54a9bf
hake <jake.p.armstrong@gmail.com>
2020-05-21 16:32:45 -0400

    added player state machine and jump animation

--- Ted/Ted.gd
+++ Ted/Ted.gd
@@ -12,10 +12,18 @@ var jump_button_timer = 0
 var allow_jump = true
 var max_jump_length_s = 1.5
 var has_landed = true
+var state
 
 onready var JumpSound = get_node("JumpSound")
 onready var PlopSound = get_node("PlopSound")
 
+enum state_list{
+	idle,
+	walking,
+	windup,
+	jumping,
+	falling,
+}
 
 func _ready() -> void:
 	moving_right = true
@@ -23,44 +31,58 @@ func _ready() -> void:
 func get_input(delta):
 	if Input.is_action_just_pressed("ui_pause"):
 		emit_signal("pause_game")
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("player_walk_right"):
+		if state == state_list.idle:
+			state = state_list.walking
+			$AnimatedSprite.play("ted_walks")
 		moving_right = true
 		if velocity.x < walk_speed:
 			velocity.x += walk_accel * delta
-		$AnimatedSprite.play("ted_walks")
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("player_walk_left"):
+		if state == state_list.idle:
+			state = state_list.walking
+			$AnimatedSprite.play("ted_walks")
 		moving_right = false
-		$AnimatedSprite.play("ted_walks")
 		if velocity.x > -walk_speed:
 			velocity.x -= walk_accel * delta
 	else:
-		$AnimatedSprite.play("ted_stands")
 		if velocity.x != 0:
-			velocity.x /= 10
-	if not moving_right:
-		$AnimatedSprite.set_flip_h(true)
-	else:
-		$AnimatedSprite.set_flip_h(false)
+			velocity.x /=  1.5
+		if state == state_list.walking:
+			state = state_list.idle
+			$AnimatedSprite.play("ted_stands")
 	if Input.is_action_pressed("player_jump") and jump_button_timer < 0.2 and allow_jump:
 		if Input.is_action_just_pressed("player_jump"):
-			JumpSound.play()
+			if state == state_list.idle or state == state_list.walking:
+				state = state_list.windup
+				$AnimatedSprite.play("ted_jump_windup")
+				JumpSound.play()
 		jump_button_timer += delta
 		velocity.y -= gravity_scale * (jump_multiplier - 1)
+	if is_on_floor() and !Input.is_action_pressed("player_jump"): 
+		allow_jump = true
+		jump_button_timer = 0
 	if jump_button_timer > max_jump_length_s:
 		allow_jump = false
 	if !is_on_floor() and !Input.is_action_pressed("player_jump"):
 		allow_jump = false
-	if just_landed():
-		PlopSound.play()
-	
+	if not moving_right:
+		$AnimatedSprite.set_flip_h(true)
+	else:
+		$AnimatedSprite.set_flip_h(false)
 
 func _physics_process(delta: float) -> void:
 	velocity.y = gravity_scale
 	get_input(delta)
+# warning-ignore:return_value_discarded
 	move_and_slide(velocity, Vector2(0, -1))
-	if is_on_floor() and !Input.is_action_pressed("player_jump"): 
-		allow_jump = true
-		jump_button_timer = 0
+	if just_landed():
+		state = state_list.idle
+		$AnimatedSprite.play("ted_stands")
+		PlopSound.play()
+	if !is_on_floor() and velocity.y > 0:
+		state = state_list.falling
+		$AnimatedSprite.play("ted_falling")
 
 func just_landed():
 	var retVal = false
@@ -70,3 +92,8 @@ func just_landed():
 	if !is_on_floor():
 		has_landed = false
 	return retVal
+
+func _on_AnimatedSprite_animation_finished() -> void:
+	if state == state_list.windup:
+		state = state_list.jumping
+		$AnimatedSprite.play("ted_jump_up")
