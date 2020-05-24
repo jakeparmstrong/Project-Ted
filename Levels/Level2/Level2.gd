9b480b36038db721d7bda090536f6487b2db8b2d
hake <jake.p.armstrong@gmail.com>
2020-05-24 11:01:31 -0400

    updated sounds

--- Levels/Level2/Level2.gd
+++ Levels/Level2/Level2.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var final_score = 0
 var bone_score = 0
+var bone_sound_index = 0
 var stop_animation = false
 var reason
 
@@ -14,9 +15,10 @@ onready var EndText = get_node("CanvasLayer/Interface/EndText")
 onready var FinalScore = get_node("CanvasLayer/Interface/FinalScore")
 onready var FinalScoreLabel = get_node("CanvasLayer/Interface/FinalScore/FinalScoreLabel")
 onready var PauseScreen = get_node("CanvasLayer/Interface/PauseScreen")
-onready var BoneCollectedSound = get_node("BoneCollected")
+onready var BoneCollectedSound = get_node("BoneCollected" + str(bone_sound_index))
 onready var YouWinSound = get_node("YouWinSound")
 onready var YouLoseSound = get_node("YouLoseSound")
+onready var UnpauseSound = get_node("UnpauseGame")
 
 enum gameover_reason {
 	stage_clear,
@@ -32,11 +34,13 @@ func _on_Bone_bone_collected() -> void:
 	bone_score += 1
 	# make setter
 	BoneCountLabel.text = ' '+str(bone_score)+"/"+str(NUM_BONES)
+	BoneCollectedSound.play()
 	if bone_score == NUM_BONES:
 		var time_remaining = ClockLabel.get_clock_time()
 		game_over(bone_score, time_remaining, gameover_reason.stage_clear)
 	else:
-		BoneCollectedSound.play()
+		bone_sound_index = (bone_sound_index + 1) % 12
+		BoneCollectedSound = get_node("BoneCollected" + str(bone_sound_index))
 
 func _on_Ted_pause_game() -> void:
 	get_tree().paused = true
@@ -45,6 +49,7 @@ func _on_Ted_pause_game() -> void:
 func _on_Interface_unpause_external() -> void:
 	get_tree().paused = false
 	PauseScreen.set_visible(false)
+	UnpauseSound.play()
 	#get_tree().reload_current_scene() #debug. take out
 
 func _on_Interface_time_out_external() -> void:
